37228694296be03904f92da2459d8e8b9d1bc289
Stanislav Bytsko <zbstof@gmail.com>
2018-06-27 11:18:18 +0300

    More stuff

--- log-spring-boot-starter/src/main/java/com/example/InitAppDebugLoggerListener.java
+++ log-spring-boot-starter/src/main/java/com/example/InitAppDebugLoggerListener.java
@@ -12,13 +12,18 @@ import org.springframework.boot.context.logging.LoggingApplicationListener;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 
-public class InitLoggerConfigListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
+public class InitAppDebugLoggerListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
 
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        if (event.getEnvironment().acceptsProfiles("debug")) {
+            String rootPackageName = ((SpringApplication) event.getSource()).getMainApplicationClass().getPackage().getName();
+            context.getConfiguration().getLoggerConfig(rootPackageName).setLevel(Level.DEBUG);
+        }
+        context.updateLoggers();
     }
 
     public int getOrder() {
-        return LoggingApplicationListener.DEFAULT_ORDER - 1;
+        return LoggingApplicationListener.DEFAULT_ORDER + 1;
     }
 }
